dbde6728656a83be6338fd9c023beb74edec2214
Ashlin Aronin & alexandra brown <student@epicodus.com>
2015-08-18 11:02:27 -0700

    Finish to-do list with categories templates and routes

--- app/app.php
+++ app/app.php
@@ -15,7 +15,9 @@
     ));
 
     $app->get("/", function() use ($app) {
-        return $app['twig']->render('index.html.twig');
+        return $app['twig']->render('index.html.twig', array(
+            'categories' => Category::getAll()
+        ));
     });
 
 
@@ -26,21 +28,35 @@
     });
 
     $app->post("/tasks", function() use ($app) {
-        $task = new Task($_POST['description']);
+        $description = $_POST['description'];
+        $category_id = $_POST['category_id'];
+        $task = new Task($description, $id = null, $category_id);
         $task->save();
-        return $app['twig']->render('tasks.html.twig', array('tasks'=> Task::getAll()));
+        $category = Category::find($category_id);
+        return $app['twig']->render('category.html.twig', array(
+            'category' => $category,
+            'tasks' => $category->getTasks()
+        ));
     });
 
 
-    // Get and post routes for categories
-    $app->get("/categories", function() use ($app) {
-        return $app['twig']->render('categories.html.twig', array('categories' => Category::getAll()));
+    // // Get and post routes for categories
+    // $app->get("/categories", function() use ($app) {
+    //     return $app['twig']->render('categories.html.twig', array('categories' => Category::getAll()));
+    // });
+
+    $app->get("/categories/{id}", function($id) use ($app) {
+        $category = Category::find($id);
+        return $app['twig']->render('category.html.twig', array(
+            'category' => $category,
+            'tasks' => $category->getTasks()
+        ));
     });
 
     $app->post("/categories", function() use ($app) {
         $category = new Category($_POST['name']);
         $category->save();
-        return $app['twig']->render('categories.html.twig', array('categories' => Category::getAll()));
+        return $app['twig']->render('index.html.twig', array('categories' => Category::getAll()));
     });
 
 
